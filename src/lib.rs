9450663227bb5aee17776efd569173d5c5a52ef6
Ofek Shaltiel <ofeks@platinum-sec.com>
2024-06-23 18:30:33 +0300

    just return the size

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,13 @@ extern crate libc;
 use libc::size_t;
 use std::os::raw::*;
 
-pub unsafe fn vsprintf_raw<V>(buffer: &mut [u8], format: *const c_char, va_list: *mut V) -> bool {
+pub unsafe fn vsprintf_raw<V>(buffer: &mut [u8], format: *const c_char, va_list: *mut V) -> i32 {
     vsnprintf_wrapper(
         buffer.as_mut_ptr(),
         buffer.len(),
         format,
         va_list as *mut c_void,
-    ) != -1
+    ) as i32
 }
 
 extern "C" {
